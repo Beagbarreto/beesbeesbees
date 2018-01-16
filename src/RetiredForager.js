954a547b38ed4c83eab044fd69696b0ec1325d38
Beagbarreto <beagbarreto09@gmail.com>
2018-01-15 18:34:28 -0600

    Completed beesbeesbees

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,12 @@
 var RetiredForagerBee = function() {
+  Forager.call(this);
+  this.age = 40;
+  this.job = "gamble";
+  this.canFly = false;
+  this.color = "grey";
+  RetiredForagerBee.prototype.forage function(){
+    return "I am too old, let me play cards instead";
+  }
 };
-
+RetiredForagerBee.prototype = Object.create(Forager.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
